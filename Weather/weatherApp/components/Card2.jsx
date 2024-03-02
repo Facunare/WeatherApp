7ee8976f9a1cd587fde6a32239138d66603d3476
facunare <arechaga.facundoet36@gmail.com>
2024-03-02 19:08:18 -0300

    media query card2

--- Weather/weatherApp/components/Card2.jsx
+++ Weather/weatherApp/components/Card2.jsx
@@ -37,7 +37,7 @@ const Card2 = () => {
 
     return (
 
-        <div className=" overflow-hidden bg-gradient-to-r from-[#372F69] via-[#3E2E73] to-[#2F2C58] backdrop-blur-md w-[390px] h-[744px] rounded-[35px] flex-col flex justify-start shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div id="card2" className=" overflow-hidden bg-gradient-to-r from-[#372F69] via-[#3E2E73] to-[#2F2C58] backdrop-blur-md w-[390px] h-[744px] rounded-[35px] flex-col flex justify-start shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
             <div className="flex justify-between px-5 mt-5">
                 <div className="flex">
                     <a href="/"><i className='bx bx-chevron-left self-center text-3xl text-[#ADA2D7]'></i></a>
